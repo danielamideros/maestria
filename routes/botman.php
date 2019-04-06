6125493f758c682dd7b77b1f887a4ea2c00b95cb
daniela.mideroso <daniela.mideroso@autonoma.edu.co>
2019-04-06 11:00:16 -0500

    Se agregó el comando de inicio

---
+++ routes/botman.php
@@ -0,0 +1,9 @@
+<?php
+use App\Http\Controllers\BotManController;
+
+$botman = resolve('botman');
+
+$botman->hears('/start', function ($bot) {
+	$nombres = $bot->getUser()->getFirstName() ?: "desconocido";
+	$bot->reply('Hola ' . $nombres . ', bienvenido al bot SimpleQuizzes!');
+});
