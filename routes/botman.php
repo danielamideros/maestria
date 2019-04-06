0cfb26ead61a203d59b04c21964860dee184438f
daniela.mideroso <daniela.mideroso@autonoma.edu.co>
2019-04-06 11:06:06 -0500

    Actualización

--- routes/botman.php
+++ routes/botman.php
@@ -11,3 +11,19 @@ $botman->hears('/start', function ($bot) {
 $botman->fallback(function ($bot) {
 	$bot->reply('No entiendo que quieres decir, vuelve a intentarlo.');
 });
+
+$botman->hears('/ayuda', function ($bot) {
+	$ayuda = ['/ayuda' => 'Mostrar este mensaje de ayuda',
+          	'acerca de|acerca' => 'Ver la información quien desarrollo este lindo bot',
+          	'listar quizzes|listar' => 'Listar los cuestionarios disponibles',
+          	'iniciar quiz <id>' => 'Iniciar la solución de un cuestionario',
+          	'ver puntajes|puntajes' => 'Ver los últimos puntajes',
+          	'borrar mis datos|borrar' => 'Borrar mis datos del sistema'];
+    
+	$bot->reply("Los comandos disponibles son:");
+
+	foreach($ayuda as $key=>$value)
+	{
+    		$bot->reply($key . ": " . $value);
+	}
+});
