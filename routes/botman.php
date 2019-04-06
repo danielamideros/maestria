5a66166405212e1a893922fdd8814f6a933365eb
daniela.mideroso <daniela.mideroso@autonoma.edu.co>
2019-04-06 11:03:05 -0500

    Actualización

--- routes/botman.php
+++ routes/botman.php
@@ -7,3 +7,7 @@ $botman->hears('/start', function ($bot) {
 	$nombres = $bot->getUser()->getFirstName() ?: "desconocido";
 	$bot->reply('Hola ' . $nombres . ', bienvenido al bot SimpleQuizzes!');
 });
+
+$botman->fallback(function ($bot) {
+	$bot->reply('No entiendo que quieres decir, vuelve a intentarlo.');
+});
